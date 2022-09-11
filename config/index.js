1c0239fab0b0f2daa3d7a6c27d1e764da2dfab0d
sunny-cuixi1 <113123389+sunny-cuixi1@users.noreply.github.com>
2022-09-11 08:24:29 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -20,7 +20,7 @@ export const config = {
    *    // 想要发送的人的名字
    *    name: "小马0",
    *    // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-   *    id: "ooOdX60J4O3NCQ3uK7YLYdsylXJU",
+   *    id: "ooOdX60J4O3NCQ3uK7YLYdsylXJU",ooOdX62AeG79MV3ffVpSfnUMPMO0
    *    // 你想对他发送的模板消息的模板ID
    *    useTemplateId: "	N1nbBocvUh9Y9HSrg4O9rxWr2KoX4UzzglA-tLBSBLM",
    *    // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
